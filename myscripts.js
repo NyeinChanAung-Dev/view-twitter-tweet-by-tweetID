aa5d35ddd8a52605f6d999df866180071a242c7a
nca <63768814+NyeinChanAung-Dev@users.noreply.github.com>
2021-08-06 11:05:07 +0630

    Add initial

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  
+  const tweetId = document.querySelector('#tweet_id').value.trim();
+  
+  if (!tweetId) return;
+  
+  document.querySelector('output').innerHTML = `
+    <br /><a href="https://twitter.com/anyuser/status/${tweetId}" target="_blank" rel="noopener noreferrer">https://twitter.com/anyuser/status/${tweetId}</a>
+  `;
+})
